02ea701d6d2ff9f30f73473d7020762c0af0a233
VosX2000 <misorecofficialutlook.com>
2024-06-27 23:31:35 -0600

    version alpha 3.1 se algunas correcciones en inicio de sesión

--- src/vista/FrmLogin.java
+++ src/vista/FrmLogin.java
@@ -8,6 +8,7 @@ import javax.swing.JOptionPane;
 public class FrmLogin extends javax.swing.JFrame {
     public FrmLogin(){
         initComponents();
+        this.setTitle("Sistema de Ventas");
     }
     /**
      * This method is called from within the constructor to initialize the form.
@@ -151,6 +152,9 @@ public class FrmLogin extends javax.swing.JFrame {
         if (verificarCredenciales(usuario, password)) {
             JOptionPane.showMessageDialog(this, "Inicio de sesión exitoso.");
             //código de redirección a la siguiente ventana.
+            FrmMenu menu = new FrmMenu();
+            menu.setVisible(true);
+            this.dispose();
         }else{
             JOptionPane.showMessageDialog(this, "Usuario o contraseñas incorrectos.", "Error de inicio de sesión.",JOptionPane.ERROR_MESSAGE);
         }
